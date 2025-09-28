02a5c568c5f4b0002371ecce249b8f7e60a40d32
Fathur Rosi <rosi.fathur@gmail.com>
2025-09-28 12:52:23 +0700

    add customer,supplier and snapshot

--- POS.Api/Controllers/CustomerController.cs
+++ POS.Api/Controllers/CustomerController.cs
@@ -1,11 +1,116 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace POS.Api.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class CustomerController : ControllerBase
+//    {
+//    }
+//}
+
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using POS.Application.Interfaces.Repositories;
+using POS.Domain.Entities;
+using POS.Domain.Models.Result;
 
 namespace POS.Api.Controllers
 {
+    [EnableCors("AllowedOrigins")]
     [Route("api/[controller]")]
     [ApiController]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class CustomerController : ControllerBase
     {
+        private readonly ILogger<CustomerController> _logger;
+        private readonly ICustomerRepository _CustomerRepository;
+
+        public CustomerController(ILogger<CustomerController> logger, ICustomerRepository CustomerRepository)
+        {
+            _logger = logger;
+            _CustomerRepository = CustomerRepository;
+        }
+
+        [HttpGet("{profile}")]
+        public ActionResult<List<Customer>> Get(string profile)
+        {
+            try
+            {
+                var results = _CustomerRepository.GetByProfile(profile);
+                if (results == null) return NotFound();
+                return this.Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return this.StatusCode(500, ex.Message);
+
+            }
+        }
+
+        [HttpGet("{code}/{profile}")]
+        public ActionResult Get(string code, string profile)
+        {
+            try
+            {
+                var results = _CustomerRepository.GetByCode(code, profile);
+                if (results == null) return NotFound();
+                return this.Ok(results);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return this.StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("Paging/{pageIndex}/{pageSize}")]
+        public async Task<ActionResult<PagingResult<Usp_GetCustomerPagingResult>>> GetDataPaging(int pageIndex = 1, int pageSize = 10)
+        {
+            try
+            {
+                var results = await _CustomerRepository.GetDataPaging(pageIndex, pageSize);
+                if (results == null) return NotFound();
+                return this.Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return this.StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer item)
+        {
+            try
+            {
+                var results = _CustomerRepository.Save(item);
+                return this.Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var results = _CustomerRepository.Delete(id);
+                return this.Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
