4f2c691308264c978052201cd1eaaa5f4bbe9112
Fathur Rosi <rosi.fathur@gmail.com>
2025-10-29 21:14:48 +0700

    submit in bulk previllege -> done

--- POS.Infrastructure/Repositories/PrevillageRepository.cs
+++ POS.Infrastructure/Repositories/PrevillageRepository.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using POS.Application.Interfaces.Repositories;
 using POS.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,10 +26,6 @@ namespace POS.Infrastructure.Repositories
 
         public async Task<List<Usp_GetPrevillageByProfileRoleResult>> GetByProfileRole(string profile, int role)
         {
-            //var profileParam = new SqlParameter("@profile", profile);
-            //var roleParam = new SqlParameter("@role", role);
-            //return _context.VUserPrevillages.FromSqlRaw("EXECUTE [Usp_GetPrevillageByProfileRole] @profile=@profile, @role=@role", profileParam, roleParam).ToList();
-
             return await _contextProc.Usp_GetPrevillageByProfileRoleAsync(profile, role);
         }
 
@@ -35,10 +33,46 @@ namespace POS.Infrastructure.Repositories
         {
             var userParam = new SqlParameter("@Username", username);
             return _context.VUserPrevillages.FromSqlRaw("EXECUTE [Usp_GetUserPrevillageByUsername] @Username", userParam).ToList();
-            //List<VUserPrevillage> items = _context.VUserPrevillages.FromSqlRaw("EXECUTE [Usp_GetUserPrevillageByUsername] @Username", userParam).ToList();
-            //return items;
-
-            //return await _contextProc.Usp_GetPrevillageByUsernameAsync(username);
         }
+
+        public async Task<object> Save(List<VRolePrevillage> items)
+        {
+            int roleId = items.FirstOrDefault() != null ? items.FirstOrDefault().RoleId : 0;
+            if (roleId == 0)
+            {
+                throw new ArgumentException("RoleId cannot be zero.");
+            }
+
+            var existingItems = await _contextProc.Usp_GetPrevillageByRoleAsync(roleId);
+
+            using var transaction = _context.Database.BeginTransaction();
+
+            try
+            {
+                foreach (var item in items)
+                {
+                    var existingItem = existingItems.Where(t => t.MenuID == item.MenuId).ToList();
+                    if (existingItem != null && existingItem.Count > 0)
+                    {
+                        await _contextProc.Usp_UpdatePrevillageAsync(item.MenuId, item.RoleId, item.AllowCreate, item.AllowRead, item.AllowUpdate, item.AllowDelete, item.AllowPrint);
+                    }
+                    else
+                    {
+                        await _contextProc.Usp_InsertPrevillageAsync(item.MenuId, item.RoleId, item.AllowCreate, item.AllowRead, item.AllowUpdate, item.AllowDelete, item.AllowPrint);
+                    }
+                }
+
+                await transaction.CommitAsync();
+
+                return new { success = true };
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                // Log the exception
+                throw;
+            }
+        }
+
     }
 }
